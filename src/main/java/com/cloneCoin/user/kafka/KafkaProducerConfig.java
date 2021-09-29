1131d1bee8f4db318dd60152c62b8cd5375f3afc
newhar <gtqh2yj@gmail.com>
2021-09-29 23:34:00 +0900

    config 수정

--- src/main/java/com/cloneCoin/user/kafka/KafkaProducerConfig.java
+++ src/main/java/com/cloneCoin/user/kafka/KafkaProducerConfig.java
@@ -18,7 +18,7 @@ public class KafkaProducerConfig {
     public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
         // kafka contatiner host
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.30.1.59:9092"); //localhost
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092"); //localhost
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
